3577a66b81123de0c23d427368855f0a32bb718f
MarlonVictor <cmarlonvictor11@gmail.com>
2023-05-19 19:05:34 -0300

    Criando a rota de autenticação com JWT

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 
 const app = fastify()
@@ -11,7 +15,12 @@ app.register(cors, {
   origin: true
 })
 
+app.register(jwt, {
+  secret: '2^uRhd7&cBQ!Le24rNc$'
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app.listen({
   port: apiPORT,
